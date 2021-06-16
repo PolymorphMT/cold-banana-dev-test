bb377b1b2ae1051142cbd7a6687719c297e6d506
Mark Tyler <marktyler1997@gmail.com>
2021-06-16 20:52:21 +0100

    WIP create skeleton structure for website
    Creating top header for title and logo, maybe side navigation too. Not focusing too much on non-functional style yet.

---
+++ cb-test-app/src/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './App.css';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <header className="App-header">
+                <p>Title</p>
+            </header>
+        )
+    }
+}
